f33cc7e87297b08b462bd3e0a0b101179b375c12
krisnasuma <45704616+krisnasuma@users.noreply.github.com>
2020-05-10 20:37:43 +0800

    Create apiservice.dart

---
+++ apiservice.dart
@@ -0,0 +1,54 @@
+import 'package:crud/src/model/profile.dart';
+import 'package:http/http.dart' show Client;
+
+class ApiService {
+  final String baseUrl = "http://api.bengkelrobot.net:8001";
+  Client client = Client();
+
+  Future<List<Profile>> getProfiles() async {
+    final response = await client.get("$baseUrl/api/profile");
+    if (response.statusCode == 200) {
+      return profileFromJson(response.body);
+    } else {
+      return null;
+    }
+  }
+
+  Future<bool> createProfile(Profile data) async {
+    final response = await client.post(
+      "$baseUrl/api/profile",
+      headers: {"content-type": "application/json"},
+      body: profileToJson(data),
+    );
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> updateProfile(Profile data) async {
+    final response = await client.put(
+      "$baseUrl/api/profile/${data.id}",
+      headers: {"content-type": "application/json"},
+      body: profileToJson(data),
+    );
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> deleteProfile(int id) async {
+    final response = await client.delete(
+      "$baseUrl/api/profile/$id",
+      headers: {"content-type": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
